c9890bdd9095ee196a47b63aeb8bea40f744faeb
Vinay Gopinath <vinayg18@gmail.com>
2016-07-25 02:46:50 +0200

    [Fix] Missing imports

--- src/components.ts
+++ src/components.ts
@@ -1,5 +1,10 @@
 import 'angular';
 import { AppComponent } from './components/app/app';
+import { SearchComponent } from './components/search/search';
+import { HistoryComponent } from './components/history/history';
+import { DisambiguationListComponent } from './components/disambiguation-list/disambiguation-list';
+import { DisambiguationComponent } from './components/disambiguation/disambiguation';
+import { ResultListComponent } from './components/result-list/result-list';
 
 export function attachComponents(app) {
   app
